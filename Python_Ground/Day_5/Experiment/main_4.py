a521d93f5428088adfa694a653d61e28c40a9933
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-11-21 12:49:51 +0600

    ninth commit

---
+++ Python_Ground/Day_5/Experiment/main_4.py
@@ -0,0 +1,24 @@
+todo_list = []
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ").strip()
+
+    match user_action:
+        case "add":
+            task = input("Enter a todo: ")
+            todo_list.append(task)
+        case 'show':
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}-{item}")
+            print("Hello", index, item)
+        case "edit":
+            todo_num = int(input("Enter todo number:  ")) - 1
+            todo_list[todo_num] = input("Enter new todo: ")
+        case "complete":
+            todo_num = int(input("Enter todo number:  ")) - 1
+            print(f"{todo_list[todo_num]} is removed")
+            todo_list.pop(todo_num)
+        case "exit":
+            break
+
+print("Bye...")
---
+++ Python_Ground/Day_5/main_2.py
@@ -0,0 +1,19 @@
+todo_list = []
+
+while True:
+    user_action = input("Type add, show, edit or exit: ").strip()
+
+    match user_action:
+        case "add":
+            task = input("Enter a todo: ")
+            todo_list.append(task)
+        case 'show':
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}-{item}")
+        case "edit":
+            todo_num = int(input("Enter todo number:  ")) - 1
+            todo_list[todo_num] = input("Enter new todo: ")
+        case "exit":
+            break
+
+print("Bye...")
