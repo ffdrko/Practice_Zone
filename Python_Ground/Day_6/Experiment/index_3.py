fb189a2a9af1e05e98dcf986489dc799afe922cc
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2024-11-22 23:55:02 +0600

    11th commit

---
+++ Python_Ground/Day_6/Experiment/index_3.py
@@ -0,0 +1,21 @@
+while True:
+    user_Action = input("Type add, show, edit, complete, exit: ").strip()
+
+    match user_Action:
+        case 'add':
+            file = open("../File/todo_list.txt", 'r')
+            todo_list = file.readlines()
+
+            todo = input("Enter a todo: ")+ '\n'
+            todo_list.append(todo)
+
+            file = open("../File/todo_list.txt", 'w')
+            file.writelines(todo_list)
+        case 'show':
+            file = open("../File/todo_list.txt", 'r')
+            todo_list = file.readlines()
+
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1} - {item}")
+        case 'exit':
+            break
